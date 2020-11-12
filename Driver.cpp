b71ac56e4eded34e38cf6a16faa2819699f1f308
Ryan Babij <rbabij@gmail.com>
2020-11-13 09:36:13 +1100

    Board output, pawns, getPieces

--- Driver.cpp
+++ Driver.cpp
@@ -9,6 +9,41 @@
 
 //complexity: 16^n
 
+// assume always promotion to queen
+
+#include <Container/Vector/Vector.hpp>
+
+#include <iostream>
+#include <string>
+
+class Piece
+{
+	std::string name;
+	std::string shortName;
+
+	public:
+	bool hasMoved;
+	bool isPromoted;
+	bool isWhite;
+
+	Piece(std::string _name, std::string _shortName)
+	{
+		name = _name;
+		shortName = _shortName;
+		hasMoved=false;
+		isPromoted=false;
+		isWhite=true;
+	}
+	std::string getName()
+	{
+		return name;
+	}
+	std::string getShortName()
+	{
+		return shortName;
+	}
+};
+
 class Board
 {
 	// 0 = empty space
@@ -27,18 +62,20 @@ class Board
 	// Q = black queen
 	// K = black king
 	
-	char aBoard [8][8];
+	Piece* aBoard [8][8];
 	bool whiteMove;
 	
 	
 	public:
 	Board()
 	{
+		std::cout<<"Board const\n";
 		//for (int 
 	}
 	
 	void reset()
 	{
+		std::cout<<"Resetting board\n";
 		whiteMove=true;
 		
 		// wipe the board
@@ -50,10 +87,12 @@ class Board
 			}
 		}
 		
+		//Piece pawn ("pawn");
+		
 		for (int i=0;i<8;++i)
 		{
-			aBoard[i][1] = 'p';
-			aBoard[i][6] = 'P';
+			aBoard[i][1] = new Piece ("pawn", "p");
+			aBoard[i][6] = new Piece("pawn", "P");
 		}
 		//aBoard[0][0] = 'r';
 		//aBoard[7][0] = 'r';
@@ -67,12 +106,84 @@ class Board
 	}
 	
 	// only return all moves for this piece, in the form of state vector
-	void getAllMovesFrom(const int x, const int y)
+	Vector <Board*>* getAllMovesFrom(const int x, const int y)
 	{
+		// unsafe array or nothing there
+		if (isSafe(x,y) == false || aBoard[x][y]==0)
+		{
+			return 0;
+		}
+		
+		Vector <Board*> * vBoard = new Vector <Board*>;
+		
+		//if (aBoard[x][y] == 'p')
+		//{
+		//}
+		//else 
+		
+		return vBoard;
+	}
+	
+	bool isSafe(const int x, const int y)
+	{
+		return (x<8 && x>0 && y<8 && y>0);
+	}
+	
+	std::string getState()
+	{
+		std::string strBoard = "";
+		for (int y=7;y>=0;--y)
+		{
+			for (int x=0;x<8;++x)
+			{
+				if ( aBoard[x][y]==0 )
+				{
+					strBoard+="-";
+				}
+				else
+				{
+					strBoard += aBoard[x][y]->getShortName();
+				}
+			}
+			strBoard+="\n";
+		}
+		return strBoard;
+	}
+	
+	Vector <Piece*>* getAllPieces(bool _isWhite)
+	{
+		Vector <Piece*>* vPieces = new Vector <Piece*>;
+		// wipe the board
+		for (int y=7;y>=0;--y)
+		{
+			for (int x=0; x<8; ++x)
+			{
+				if (aBoard[x][y]!=0)
+				{
+					if (aBoard[x][y]->isWhite == _isWhite)
+					{
+						vPieces->push(aBoard[x][y]);
+					}
+				}
+			}
+		}
+		
+		if (vPieces->size() == 0)
+		{
+			delete vPieces;
+		}
+		
+		return vPieces;
 	}
 };
 
 int main (int narg, char ** arg)
 {
+	
+	Board mainBoard;
+	mainBoard.reset();
+
+	std::cout<<"Board state:\n"<<mainBoard.getState()<<"\n\n";
+	
 	return 0;
 }
